df9726580b90ccc502537809c894048dbf374f79
Diego Rossi <diego.rossi@yahoo.com.br>
2020-06-23 11:07:57 -0300

    Aula 2
    Aula do dia 23/06

---
+++ aula2/js/main.js
@@ -0,0 +1,17 @@
+/*
+    Código da aula 2
+    Comandos de Entrada e saída
+*/
+
+//variável
+let nomeCompleto, resposta
+
+//Entrada
+nomeCompleto = prompt('Digite o seu nome:')
+resposta = confirm('Você deseja aprofundar em JavaScript?')
+
+console.log(resposta)
+//Saída
+alert("Olá mundo "+nomeCompleto)
+document.write('Sejam bem-vindos')
+console.log('Ok')
